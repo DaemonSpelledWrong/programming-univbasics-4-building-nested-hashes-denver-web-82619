07ce6e281760ecd9bfd5464da75b9abe267758a5
Damon Self <damongself@gmail.com>
2019-07-20 11:21:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,10 @@ def monopoly_with_second_tier
         {
           "mortgage_value" => "$100"
         },
+        :pennsylvania_railroad =>
+        {
+          "mortgage_value" => "$200"
+        },
         
       }
     }
